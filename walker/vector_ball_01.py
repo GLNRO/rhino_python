0bacd43dd0af8ad7e2a93c95aeeb0e8d965450b8
GLNRO <LG@GLNRO.com>
2015-11-13 23:09:20 -0500

    Create moving script for moving sphere (2d)

---
+++ walker/vector_ball_01.py
@@ -0,0 +1,20 @@
+﻿import rhinoscriptsyntax as rs
+
+
+xPos = 5
+yPos = 5
+xSpeed = 5
+ySpeed = 8
+
+boundary = 90
+
+for t in range(time):
+    xPos += xSpeed
+    yPos += ySpeed
+    
+    if xPos < 0 or xPos > boundary:
+        xSpeed *= -1
+    if yPos < 0 or yPos > boundary:
+        ySpeed *= -1
+        
+a = rs.AddSphere([xPos,yPos,0],5)
